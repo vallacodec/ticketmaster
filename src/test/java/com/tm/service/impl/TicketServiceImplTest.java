2a654d2bd3c9266204e62a01b3dfcb0111c73e45
Vallaban <svallaban1@uspto.gov>
2015-11-04 16:40:37 -0500

    initial commit for the ticket service

---
+++ src/test/java/com/tm/service/impl/TicketServiceImplTest.java
@@ -0,0 +1,37 @@
+package com.tm.service.impl;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by svallaban1 on 11/3/2015.
+ */
+public class TicketServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testNumSeatsAvailable() throws Exception {
+
+    }
+
+    @Test
+    public void testFindAndHoldSeats() throws Exception {
+
+    }
+
+    @Test
+    public void testReserveSeats() throws Exception {
+
+    }
+}
